391a190c7a678fc125d9e54c6a2edc589800365e
Cheolkie Kim <ckiekim@naver.com>
2022-09-13 11:51:59 +0900

    함수

--- 03.JavaScript/ch05.함수/03.매개변수초기화.js
+++ 03.JavaScript/ch05.함수/03.매개변수초기화.js
@@ -3,9 +3,18 @@ function print(name, count) {
     /* if (!count)
         count = 0; */
     count = count ? count : 0;
-    // count = count || 0;
+    // count = count || 0;              // ES-11
     console.log(`${name}이/가 ${count}개 있습니다.`);
 }
 
 print('사과', 10);
 print('감');
+
+function print2(name, count=0) {        // ES-6
+    /* if (!count)
+        count = 0; */
+    //count = count ? count : 0;
+    // count = count || 0;
+    console.log(`${name}이/가 ${count}개 있습니다.`);
+}
+print2('감');
